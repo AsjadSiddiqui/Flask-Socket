35a34e1eb7c0071456073c8fe3fd6e986ea0c9b3
Asjad <asjadsidd101@gmail.com>
2020-07-16 16:24:04 +0500

    Init

--- restful.py
+++ restful.py
@@ -6,5 +6,14 @@ app.route("/", methods=["GET"])
 def main():
   return jsonify({"msg": "Hello World !"})
 
+app.route("/home/", methods=["GET"])
+def main():
+  return jsonify({"msg": "Hello World !"})
+
+app.route("/test/", methods=["GET"])
+def main():
+  return "Hello World !"
+
+
 if __name__ == "__main__":
   app.run()
